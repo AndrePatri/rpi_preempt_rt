30c22920fec38f853ff925d6ccd98fd9855b553d
andrea <andreapatrizi1b6e6@gmail.com>
2022-08-06 13:25:49 +0200

    mojn

--- setup_crosscomp.bash
+++ setup_crosscomp.bash
@@ -4,17 +4,61 @@ sudo apt install gcc make gcc-arm-linux-gnueabi binutils-arm-linux-gnueabi
 sudo apt-get install libncurses-dev flex bison openssl libssl-dev dkms libelf-dev libudev-dev libpci-dev libiberty-dev autoconf fakeroot
 
 sudo apt-get install libncurses-dev gawk flex bison openssl libssl-dev dkms libelf-dev libudev-dev libpci-dev libiberty-dev autoconf
+sudo apt-get build-dep linux
 
 cd ~
-mkdir kernel_ws2
-cd kernel_ws2
+mkdir kernel_ws
+mkdir kernel_ws/src
+
+git clone git@github.com:AndPatr/rpi_preempt_rt.git
 
 wget https://mirrors.edge.kernel.org/pub/linux/kernel/v5.x/linux-5.15.55.tar.gz
 
 wget https://cdn.kernel.org/pub/linux/kernel/projects/rt/5.15/patch-5.15.55-rt48.patch.gz
 
 tar -xzf linux-5.15.55.tar.gz
+gunzip patch-5.15.55-rt48.patch.gz
 
 cd linux-5.15.55
 
 patch -p1 < ../patch-5.15.55-rt48
+
+yes '' | make oldconfig
+
+make menuconfig
+
+
+###
+
+# Enable CONFIG_PREEMPT_RT
+ -> General Setup
+  -> Preemption Model (Fully Preemptible Kernel (Real-Time))
+   (X) Fully Preemptible Kernel (Real-Time)
+
+# Enable CONFIG_HIGH_RES_TIMERS
+ -> General setup
+  -> Timers subsystem
+   [*] High Resolution Timer Support
+
+# Enable CONFIG_NO_HZ_FULL
+ -> General setup
+  -> Timers subsystem
+   -> Timer tick handling (Full dynticks system (tickless))
+    (X) Full dynticks system (tickless)
+
+# Set CONFIG_HZ_1000 (note: this is no longer in the General Setup menu, go back twice)
+ -> Processor type and features
+  -> Timer frequency (1000 HZ)
+   (X) 1000 HZ
+
+# Set CPU_FREQ_DEFAULT_GOV_PERFORMANCE [=y]
+ ->  Power management and ACPI options
+  -> CPU Frequency scaling
+   -> CPU Frequency scaling (CPU_FREQ [=y])
+    -> Default CPUFreq governor (<choice> [=y])
+     (X) performance
+
+
+###
+make ARCH=aarch64 CROSS_COMPILE=aarch64-linux-gnu-gcc
+
