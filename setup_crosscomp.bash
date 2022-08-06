2ea1e519f1d79d7327dad9aef8e5851bf75af212
andrea <andreapatrizi1b6e6@gmail.com>
2022-08-06 19:35:49 +0200

    change

--- setup_crosscomp.bash
+++ setup_crosscomp.bash
@@ -1,4 +1,6 @@
 #!/bin/bash
+path=$(realpath “${BASH_SOURCE:-$0}”)
+
 sudo apt install gcc make gcc-arm-linux-gnueabi binutils-arm-linux-gnueabi
 
 sudo apt-get install libncurses-dev flex bison openssl libssl-dev dkms libelf-dev libudev-dev libpci-dev libiberty-dev autoconf fakeroot
@@ -6,59 +8,59 @@ sudo apt-get install libncurses-dev flex bison openssl libssl-dev dkms libelf-de
 sudo apt-get install libncurses-dev gawk flex bison openssl libssl-dev dkms libelf-dev libudev-dev libpci-dev libiberty-dev autoconf
 sudo apt-get build-dep linux
 
-cd ~
-mkdir kernel_ws
-mkdir kernel_ws/src
+cd path
 
-git clone git@github.com:AndPatr/rpi_preempt_rt.git
+# git clone git@github.com:AndPatr/rpi_preempt_rt.git
 
-wget https://mirrors.edge.kernel.org/pub/linux/kernel/v5.x/linux-5.15.55.tar.gz
+wget https://mirrors.edge.kernel.org/pub/linux/kernel/v5.x/linux-5.15.55.tar.gz .
 
-wget https://cdn.kernel.org/pub/linux/kernel/projects/rt/5.15/patch-5.15.55-rt48.patch.gz
+wget https://cdn.kernel.org/pub/linux/kernel/projects/rt/5.15/patch-5.15.55-rt48.patch.gz .
 
 tar -xzf linux-5.15.55.tar.gz
 gunzip patch-5.15.55-rt48.patch.gz
 
 cd linux-5.15.55
 
-patch -p1 < ../patch-5.15.55-rt48
+cp config-5.15.0-1012-raspi .config
+
+# patch -p1 < ../patch-5.15.55-rt48
 
 yes '' | make oldconfig
 
-make menuconfig
+# make menuconfig
 
 
 ###
 
-# Enable CONFIG_PREEMPT_RT
- -> General Setup
-  -> Preemption Model (Fully Preemptible Kernel (Real-Time))
-   (X) Fully Preemptible Kernel (Real-Time)
+# # Enable CONFIG_PREEMPT_RT
+#  -> General Setup
+#   -> Preemption Model (Fully Preemptible Kernel (Real-Time))
+#    (X) Fully Preemptible Kernel (Real-Time)
 
-# Enable CONFIG_HIGH_RES_TIMERS
- -> General setup
-  -> Timers subsystem
-   [*] High Resolution Timer Support
+# # Enable CONFIG_HIGH_RES_TIMERS
+#  -> General setup
+#   -> Timers subsystem
+#    [*] High Resolution Timer Support
 
-# Enable CONFIG_NO_HZ_FULL
- -> General setup
-  -> Timers subsystem
-   -> Timer tick handling (Full dynticks system (tickless))
-    (X) Full dynticks system (tickless)
+# # Enable CONFIG_NO_HZ_FULL
+#  -> General setup
+#   -> Timers subsystem
+#    -> Timer tick handling (Full dynticks system (tickless))
+#     (X) Full dynticks system (tickless)
 
-# Set CONFIG_HZ_1000 (note: this is no longer in the General Setup menu, go back twice)
- -> Processor type and features
-  -> Timer frequency (1000 HZ)
-   (X) 1000 HZ
+# # Set CONFIG_HZ_1000 (note: this is no longer in the General Setup menu, go back twice)
+#  -> Processor type and features
+#   -> Timer frequency (1000 HZ)
+#    (X) 1000 HZ
 
-# Set CPU_FREQ_DEFAULT_GOV_PERFORMANCE [=y]
- ->  Power management and ACPI options
-  -> CPU Frequency scaling
-   -> CPU Frequency scaling (CPU_FREQ [=y])
-    -> Default CPUFreq governor (<choice> [=y])
-     (X) performance
+# # Set CPU_FREQ_DEFAULT_GOV_PERFORMANCE [=y]
+#  ->  Power management and ACPI options
+#   -> CPU Frequency scaling
+#    -> CPU Frequency scaling (CPU_FREQ [=y])
+#     -> Default CPUFreq governor (<choice> [=y])
+#      (X) performance
 
 
-###
-make ARCH=aarch64 CROSS_COMPILE=aarch64-linux-gnu-gcc
+# ###
+# make ARCH=aarch64 CROSS_COMPILE=aarch64-linux-gnu-gcc 
 
