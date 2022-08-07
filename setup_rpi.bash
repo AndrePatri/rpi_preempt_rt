40e6a2da43cb40f7d56b1b67ef72a3381f232958
andrea patrizi <andrepatrizi1b6e6@gmail.com>
2022-08-07 22:00:04 +0200

    erberb

---
+++ setup_rpi.bash
@@ -0,0 +1,22 @@
+#!/bin/bash
+path="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+
+cd ~
+
+github_mail=andreapatrizi1b6e6@gmail.com
+ssh-keygen -t ed25519 -C "$github_mail"
+
+echo "Ssh key generated."
+echo "Add the following public key to your GitHub SSH keys and then insert \" OK \" to proceed with the setup."
+
+while read line; do echo $line; done < .ssh/id_ed25519.pub
+
+git clone  git@github.com:AndPatr/rpi_preempt_rt.git
+
+cd $path
+
+sudo apt update
+
+sudo apt upgrade
+
+#./install_kern.bash
