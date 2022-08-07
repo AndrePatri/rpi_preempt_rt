2efdea186e476b5873a8d2a8439c64a94de9b149
andrea patrizi <andrepatrizi1b6e6@gmail.com>
2022-08-07 22:13:52 +0200

    vsvbrfikjhgfdsa

--- setup_rpi.bash
+++ setup_rpi.bash
@@ -3,16 +3,26 @@ path="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
 cd ~
 
-git clone git@github.com:AndPatr/rpi_preempt_rt.git
+set -e
 
 github_mail=andreapatrizi1b6e6@gmail.com
 ssh-keygen -t ed25519 -C "$github_mail"
 
 echo "Ssh key generated."
 echo "Add the following public key to your GitHub SSH keys and then insert \" OK \" to proceed with the setup."
-
 while read line; do echo $line; done < .ssh/id_ed25519.pub
 
+while read -n 1 -p "Input Selection:" user_input
+do 
+    if [ $user_input -eq "OK" ]; then
+        
+        break
+
+    fi
+
+done
+
+git clone git@github.com:AndPatr/rpi_preempt_rt.git
 
 cd $path
 
