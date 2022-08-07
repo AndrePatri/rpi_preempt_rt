90af08b1dd8549f4429521d2cc3b32ff388e1801
andrea patrizi <andrepatrizi1b6e6@gmail.com>
2022-08-07 23:01:39 +0200

    add purple

--- setup_rpi.bash
+++ setup_rpi.bash
@@ -5,6 +5,7 @@ GREEN='\033[0;32m'
 ORANGE='\033[0;33m'
 BLUE='\033[0;34m'
 CYAN='\033[0;36m'
+PURPLE='\033[0;35m'
 NC='\033[0m' # No Color
 
 path="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
