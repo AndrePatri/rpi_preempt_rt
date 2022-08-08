cb9cd007153974b73a8c7ad94989a637455df08c
andrea patrizi <andrepatrizi1b6e6@gmail.com>
2022-08-08 07:29:05 +0200

    write config to usrcfg.txt

--- install_kern.bash
+++ install_kern.bash
@@ -32,42 +32,14 @@ cp /boot/config-$KERN /boot/firmware/$KERN/
 cp /boot/initrd.img-$KERN /boot/firmware/$KERN/
 cp /boot/firmware/config.txt{,.bak}
 # sudo cp /boot/cmdline.txt /boot/firmware/$KERN/
-cat > /boot/firmware/config.txt << EOF
+cat > /boot/firmware/usrcfg.txt << EOF
 
 [pi4]
-max_framebuffers=2
-
-[all]
-cmdline=cmdline.txt
-
-# Enable the audio output, I2C and SPI interfaces on the GPIO header
-dtparam=audio=on
-dtparam=i2c_arm=on
-dtparam=spi=on
-
-# Enable the serial pins
-enable_uart=1
-
-# Comment out the following line if the edges of the desktop appear outside
-# the edges of your display
-disable_overscan=1
-
-# If you have issues with audio, you may try uncommenting the following line
-# which forces the HDMI output into HDMI mode instead of DVI (which doesn't
-# support audio output)
-#hdmi_drive=2
-
-# If you have a CM4, uncomment the following line to enable the USB2 outputs
-# on the IO board (assuming your CM4 is plugged into such a board)
-#dtoverlay=dwc2,dr_mode=host
-
-[all]
 kernel=vmlinuz-$KERN
 initramfs initrd.img-$KERN
 os_prefix=$KERN/
-overlay_prefix=overlays/$(if [[ "$KERN" =~ 'v8' ]]; then echo -e "\narm_64bit=1"; fi)
 
-[all]
+[pi4]
 dtoverlay=$can_hat_name
 
 EOF
