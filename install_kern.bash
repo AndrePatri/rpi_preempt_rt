28b7e4e4b7e2a94b1874006a4db2a061fef6772b
andrea patrizi <andrepatrizi1b6e6@gmail.com>
2022-08-08 09:47:53 +0200

    shjhgtfred

--- install_kern.bash
+++ install_kern.bash
@@ -20,9 +20,9 @@ apt install ./linux-image-$kern_full_name-v8_$kern_full_name-v8-1_arm64.deb
 
 dpkg -i *.deb
 
-mkdir -p /boot/firmware/$KERN/overlays/
-sudo cp -d /usr/lib/linux-image-$KERN/overlays/* /boot/firmware/$KERN/overlays/
-# cp -dr /usr/lib/linux-image-$KERN/* /boot/firmware/$KERN/
+-p /boot/firmware/$KERN/overlays/
+cp -d /usr/lib/linux-image-$KERN/overlays/* /boot/firmware/$KERN/overlays/
+cp -dr /usr/lib/linux-image-$KERN/* /boot/firmware/$KERN/
 [[ -d /usr/lib/linux-image-$KERN/broadcom ]] && cp -d /usr/lib/linux-image-$KERN/broadcom/* /boot/firmware/$KERN/
 
 touch /boot/firmware/$KERN/overlays/README
